73b036e37819eca405140434c6c0d27c255b6d24
Gustavo <gustavo.it@outlook.com>
2021-12-14 09:59:47 -0300

    Feat
    End of edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,39 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if (newTaskTitle === null) return
+    if (newTaskTitle === '') return
+    if (newTaskTitle === undefined) return
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const selectedTask = tasks.find((task) => task.id === id)
+    if (!selectedTask) return
+    const clonedTasks = JSON.parse(JSON.stringify(tasks));
+    const selectedTaskClone = clonedTasks.find((task: Task) => task.id === id)
+    const indexTask = clonedTasks.indexOf(selectedTaskClone)
+    clonedTasks[indexTask].done = !clonedTasks[indexTask].done
+    setTasks(clonedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const taskToRemove = tasks.find((task) => task.id === id)
+    if (!taskToRemove) return
+    const newTaskList = tasks.filter((task) => task.id !== id)
+
+    setTasks(newTaskList)
   }
 
   return (
